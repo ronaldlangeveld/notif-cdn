a1557cc72dbc17ae185a1eff08034089b1c8f5c9
ronald <ronaldlangeveld@gmail.com>
2019-08-14 13:47:28 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+var getJSON = function (url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+  };
+
+  var domain = window.location.hostname;
+
+  var getLink = "http://127.0.0.1:3500/testingcors/" + domain
+
+  if (domain) {
+    try {
+
+      getJSON(getLink,
+        function (err, data) {
+          if (err !== null) {
+            console.log(err);
+          } else {
+            var BoxAtt = data.data;
+            var el = document.querySelector('body');
+            var htmlBox = '<div class="boxBarTop" style="background-color: ' + BoxAtt.backgroundColor + ';"> <p class="boxText" style="color:' + BoxAtt.textColor + '"> ' + BoxAtt.text + ' </p></div>'
+            el.innerHTML =  htmlBox + el.innerHTML;
+          }
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+
+    pass
+
+  }
+
